b5f8a016e190845f452770f8a8792e36fd3634a1
Mo-Gee <109600499+Mo-Gee@users.noreply.github.com>
2025-05-15 23:08:12 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -43,7 +43,7 @@ app.use('/api/announcements', announcementRoutes);
 
 // Connect to MongoDB database using connection string from environment variables
 console.log(process.env.MONGO_URL)       // Log the MongoDB connection string 
-if (!MONGODB_URI) {
+if (!process.env.MONGODB_URL) {
   console.error('MONGODB_URI environment variable is not set!');
   process.exit(1); // Exit the application if the URI is missing
 }
