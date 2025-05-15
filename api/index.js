0473225b33d5f33d94dc85183661af24ada15bdc
Mo-Gee <109600499+Mo-Gee@users.noreply.github.com>
2025-05-15 23:04:13 +0200

    Update index.js
    Added some error handling for the DB connection

--- api/index.js
+++ api/index.js
@@ -43,6 +43,10 @@ app.use('/api/announcements', announcementRoutes);
 
 // Connect to MongoDB database using connection string from environment variables
 console.log(process.env.MONGO_URL)       // Log the MongoDB connection string 
+if (!MONGODB_URI) {
+  console.error('MONGODB_URI environment variable is not set!');
+  process.exit(1); // Exit the application if the URI is missing
+}
 mongoose.connect(process.env.MONGO_URL);
 //AnsovHSTtu42dPLS
 
